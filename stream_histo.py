473b7a8fb43f7e025b63e60f0a0064d099193966
impejonathan <impe.jonathan@gmail.com>
2023-04-12 14:22:26 +0200

    la carte streamlit

--- stream_histo.py
+++ stream_histo.py
@@ -2,12 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Définition de la liste des mois en français
-mois_fr = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
-
-# Définition de la liste des gares
-gares = ["Paris Bercy", "Paris Saint-Lazare", "Paris Gare de Lyon", "Paris Gare du Nord", "Paris Montparnasse", "Paris Est", "Paris Austerlitz"]
-
 # lire le fichier CSV dans un DataFrame pandas
 df = pd.read_csv("objets-trouves.csv")
 
@@ -23,6 +17,15 @@ start_date = pd.Timestamp("2019-01-01")
 end_date = pd.Timestamp("2022-12-31")
 df = df[(df["date"] >= start_date) & (df["date"] <= end_date)]
 
+# récupérer les types d'objets uniques
+types_objets = df["type d'objet"].unique()
+
+# Définition de la liste des mois en français
+mois_fr = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
+
+# Définition de la liste des gares
+gares = ["Paris Bercy", "Paris Saint-Lazare", "Paris Gare de Lyon", "Paris Gare du Nord", "Paris Montparnasse", "Paris Est", "Paris Austerlitz"]
+
 # Ajout du sélecteur pour l'année et le mois
 annee = st.selectbox("Choisissez une année", options=range(2019, 2023), index=3)  # valeur par défaut : 2022
 mois = st.selectbox("Choisissez un mois", options=mois_fr, index=0)  # valeur par défaut : janvier
@@ -30,11 +33,19 @@ mois = st.selectbox("Choisissez un mois", options=mois_fr, index=0)  # valeur pa
 # Ajout du sélecteur pour la gare
 gare = st.selectbox("Choisissez une gare", options=gares)
 
+# Ajout du sélecteur pour le type d'objet
+select_type_objet = st.selectbox("Choisissez un type d'objet (optionnel)", options=["Tous"] + list(types_objets))
+if select_type_objet != "Tous":
+    df = df[df["type d'objet"] == select_type_objet]
+
 # Filtrer les données en fonction de l'année, du mois et de la gare sélectionnés
 filtered_data = df[(df["annee"] == annee) & (df["mois"] == mois_fr.index(mois) + 1) & (df["gare"] == gare)]
 
 # Affichage du nombre d'objets perdus dans le titre
-title = f"Nombre d'objets perdus en {mois} {annee} à la gare {gare} : {len(filtered_data)}"
+title = f"Nombre d'objets perdus en {mois} {annee} à la gare {gare}"
+if select_type_objet != "Tous":
+    title += f" pour le type d'objet : {select_type_objet}"
+title += f" il y à  {len(filtered_data)} objets"
 st.title(title)
 
 # Affichage de l'histogramme dans Streamlit
