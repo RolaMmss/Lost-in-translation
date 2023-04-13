8193b5a628804565234faac0d496b899c9b4bf3a
impejonathan <impe.jonathan@gmail.com>
2023-04-13 14:42:31 +0200

    correction histo bar

--- stream_histo.py
+++ stream_histo.py
@@ -21,17 +21,17 @@ df = df[(df["date"] >= start_date) & (df["date"] <= end_date)]
 types_objets = df["type d'objet"].unique()
 
 # Définition de la liste des mois en français
-mois_fr = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
+mois_fr = ["Tous", "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
 
 # Définition de la liste des gares
-gares = ["Paris Bercy", "Paris Saint-Lazare", "Paris Gare de Lyon", "Paris Gare du Nord", "Paris Montparnasse", "Paris Est", "Paris Austerlitz"]
+gares = ["Tous", "Paris Bercy", "Paris Saint-Lazare", "Paris Gare de Lyon", "Paris Gare du Nord", "Paris Montparnasse", "Paris Est", "Paris Austerlitz"]
 
 # Ajout du sélecteur pour l'année et le mois
 annee = st.selectbox("Choisissez une année", options=range(2019, 2023), index=3)  # valeur par défaut : 2022
-mois = st.selectbox("Choisissez un mois", options=mois_fr, index=0)  # valeur par défaut : janvier
+mois = st.selectbox("Choisissez un mois", options=mois_fr, index=0)  # valeur par défaut : Tous
 
 # Ajout du sélecteur pour la gare
-gare = st.selectbox("Choisissez une gare", options=gares)
+gare = st.selectbox("Choisissez une gare", options=gares, index=0)  # valeur par défaut : Tous
 
 # Ajout du sélecteur pour le type d'objet
 select_type_objet = st.selectbox("Choisissez un type d'objet (optionnel)", options=["Tous"] + list(types_objets))
@@ -39,7 +39,14 @@ if select_type_objet != "Tous":
     df = df[df["type d'objet"] == select_type_objet]
 
 # Filtrer les données en fonction de l'année, du mois et de la gare sélectionnés
-filtered_data = df[(df["annee"] == annee) & (df["mois"] == mois_fr.index(mois) + 1) & (df["gare"] == gare)]
+if mois == "Tous" and gare == "Tous":
+    filtered_data = df[(df["annee"] == annee)]
+elif mois == "Tous":
+    filtered_data = df[(df["annee"] == annee) & (df["gare"] == gare)]
+elif gare == "Tous":
+    filtered_data = df[(df["annee"] == annee) & (df["mois"] == mois_fr.index(mois))]
+else:
+    filtered_data = df[(df["annee"] == annee) & (df["mois"] == mois_fr.index(mois)) & (df["gare"] == gare)]
 
 # Affichage du nombre d'objets perdus dans le titre
 title = f"Nombre d'objets perdus en {mois} {annee} à la gare {gare}"
